01bc3e3a10316aa754dfc77444ea4e3caee1e9e5
Alexander <alez.grante@gmail.com>
2022-03-24 20:07:24 +0300

    Initial commit

---
+++ app/src/androidTest/java/com/example/noapp/ExampleInstrumentedTest.kt
@@ -0,0 +1,34 @@
+package com.example.noapp
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+import org.junit.Rule
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class ExampleInstrumentedTest {
+
+    @get:Rule
+    val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun greeterSaysHello() {
+        onView(withId(R.id.textview)).check(matches(withText("Hello World!")))
+    }
+
+}
